c4bdc0ab55f8e2d2e097056e061feaafddd5798d
induwaraumesh6-svg <induwaraumesh6@gmail.com>
2026-05-18 15:39:27 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+
+function sendOrder(e){
+e.preventDefault();
+document.getElementById("msg").innerHTML="✅ Order Submitted Successfully!";
+}
+
+
+function createAccount(e){
+e.preventDefault();
+document.getElementById("accountmsg").innerHTML =
+"✅ Account created successfully!";
+}
